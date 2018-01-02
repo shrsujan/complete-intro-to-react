8938354e3f147ac99f302281e1ec3b9f57f5aaac
Sujan Shrestha <shrsujan2007@gmail.com>
2018-01-02 12:33:01 +0545

    tutorial: Redux review

--- js/App.jsx
+++ js/App.jsx
@@ -1,30 +1,36 @@
 // @flow
 
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import type { Match } from 'react-router-dom';
-import Landing from './Landing';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import store from './store';
 import Search from './Search';
 import Details from './Detais';
+import Landing from './Landing';
+
 import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
-        <Route
-          path="/details/:id"
-          component={(props: { match: Match }) => (
-            <Details show={preload.shows.find(show => props.match.params.id === show.imdbID)} {...props} />
-          )}
-        />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
+          <Route
+            path="/details/:id"
+            component={(props: { match: Match }) => (
+              <Details show={preload.shows.find(show => props.match.params.id === show.imdbID)} {...props} />
+            )}
+          />
+          <Route component={FourOhFour} />
+        </Switch>
+      </div>
+    </Provider>
   </BrowserRouter>
 );
 
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,24 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Landing = () => (
+import {setSearchTerm} from './actionCreators';
+
+const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
   <div className="landing">
     <h1>svideo</h1>
-    <input type="text" placeholder="Search" />
+    <input type="text" placeholder="Search" value={props.searchTerm} onChange={props.handleSearchTermChange} />
     <Link to="/search">or Browse All</Link>
   </div>
 );
 
-export default Landing;
+const mapStateToProps = state => ({ searchTerm: state.searchTerm });
+const mapDispatchToProps = (dispatch: Function) => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm(event.target.value))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
